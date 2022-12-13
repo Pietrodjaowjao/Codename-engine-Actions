7bc35f8e7e1409baec2c3f16736c440161138c13
Pietronicolas <77608535+Pietrodjaowjao@users.noreply.github.com>
2022-12-13 02:57:35 -0300

    Initial commit

---
+++ source/flx3d/Flx3DView.hx
@@ -0,0 +1,80 @@
+package flx3d;
+
+import away3d.loaders.misc.AssetLoaderToken;
+import away3d.library.assets.Asset3DType;
+import away3d.events.Asset3DEvent;
+import away3d.loaders.parsers.OBJParser;
+import away3d.utils.Cast;
+import away3d.materials.TextureMaterial;
+import haxe.io.Path;
+import away3d.loaders.misc.AssetLoaderContext;
+import openfl.Assets;
+import away3d.entities.Mesh;
+import away3d.loaders.Loader3D;
+
+// FlxView3D with helpers for easier updating
+
+class Flx3DView extends FlxView3D {
+    public function initLoader() {
+        if (_loader != null) return;
+        _loader = new Loader3D();
+        _loader.addEventListener(Asset3DEvent.ASSET_COMPLETE, onAssetComplete);
+        view.scene.addChild(_loader);
+    }
+
+    private var meshCallback:Mesh->Void = null;
+
+    private var queue:Array<MeshQueueItem> = [];
+
+    private function onAssetComplete(event:Asset3DEvent) {
+        if (event.asset != null && event.asset.assetType == Asset3DType.MESH) {
+            var mesh:Mesh = cast(event.asset, Mesh);
+            if (meshCallback != null)
+                meshCallback(mesh);
+            meshCallback = null;
+            if (queue.length > 0) {
+                var m = queue.shift();
+                addMesh(m.assetPath, m.callback, m.texturePath);
+            }
+        }
+    }
+
+	public function addMesh(assetPath:String, callback:Mesh->Void, ?texturePath:String, smoothTexture:Bool = true) {
+        initLoader();
+
+        var model = Assets.getBytes(assetPath);
+        if (model == null)
+            throw 'Model at ${assetPath} was not found.';
+        if (meshCallback != null) {
+            queue.push({
+                assetPath: assetPath,
+                callback: callback,
+                texturePath: texturePath
+            });
+            return null;
+        }
+
+        var context = new AssetLoaderContext();
+        var noExt = Path.withoutExtension(assetPath);
+        trace(noExt);
+        context.mapUrlToData('${Path.withoutDirectory(noExt)}.mtl', '$noExt.mtl');
+
+        var material:TextureMaterial = null;
+        if (texturePath != null)
+            material = new TextureMaterial(Cast.bitmapTexture(texturePath), smoothTexture);
+
+        var token = _loader.loadData(model, context, null, new OBJParser());
+        meshCallback = function(mesh) {
+            if (material != null)
+                mesh.material = material;
+            callback(mesh);
+        };
+        return token;
+	}
+}
+
+typedef MeshQueueItem = {
+    var assetPath:String;
+    var callback:Mesh->Void;
+    var texturePath:String;
+}
